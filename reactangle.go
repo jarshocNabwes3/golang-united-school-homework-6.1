4ae2c273c23a23e161ea9a664bc1ccd47a502667
Peter Vereshagin <peter@vereshagin.org>
2022-05-26 18:31:03 +0000

    feat: :white_check_mark: estBox_AddShape_ErrAddMoreThanMax() pass

--- reactangle.go
+++ reactangle.go
@@ -5,3 +5,19 @@ type Rectangle struct {
 	Height, Weight float64
 }
 
+func (rectangle *Rectangle) CalcArea() (area float64) {
+	Height := rectangle.Height
+	Weight := rectangle.Weight
+	area = Height * Weight
+
+	return
+}
+
+func (rectangle *Rectangle) CalcPerimeter() (perimeter float64) {
+	Height := rectangle.Height
+	Weight := rectangle.Weight
+
+	perimeter = (Weight + Height) * 2
+
+	return
+}
--- triangle.go
+++ triangle.go
@@ -1,6 +1,22 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (triangle *Triangle) CalcArea() (area float64) {
+	side := triangle.Side
+	area = math.Sqrt(.75) * side * side
+
+	return
+}
+
+func (triangle *Triangle) CalcPerimeter() (perimeter float64) {
+	side := triangle.Side
+	perimeter = side * 3
+
+	return
 }
